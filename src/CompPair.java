cb2c760af86e91ca6151c415b742df7c86570f88
mohammedaldiri <aldiri.mk@gmail.com>
2024-03-21 01:32:06 +0300

    Start Working

--- src/CompPair.java
+++ src/CompPair.java
@@ -1,5 +1,5 @@
 // Class used to store a pair of elements that is comparable on the second element.
-//START HERE PLEASE
+//START HERE PLEASE Hello World
 public class CompPair<U, V extends Comparable<V>> extends Pair<U, V> implements Comparable<CompPair<U, V>> {
 	public CompPair(U first, V second) {
 		super(first, second);
