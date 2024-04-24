12600eda1ab221effb084e60845087d9d88eee45
mohammedaldiri <aldiri.mk@gmail.com>
2024-04-24 22:25:47 +0300

    H

--- src/BST.java
+++ src/BST.java
@@ -13,11 +13,45 @@ public class BST<T> implements Map{
         return root == null;
     }
 
+    @Override
+    public void clear() {
+
+    }
 
 
     public T retrieve ()
     { // still not completed it should return the key also not just the data
         return current.data;
+    }
+
+    @Override
+    public void update(Object e) {
+
+    }
+
+    @Override
+    public boolean find(Comparable key) {
+        return false;
+    }
+
+    @Override
+    public int nbKeyComp(Comparable key) {
+        return 0;
+    }
+
+    @Override
+    public boolean insert(Comparable key, Object data) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Comparable key) {
+        return false;
+    }
+
+    @Override
+    public DLLComp getKeys() {
+        return null;
     }
 
     public int size()
